9dd8160c8de7b6c194d9e215e57a424ea6b430ee
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-26 11:42:16 +0000

    Add basic app that loads master.honesty.store site
    Uses status bar plugin to prevent web view from being positioned
    underneath (is instead offset).

--- app/platforms/ios/platform_www/cordova_plugins.js
+++ app/platforms/ios/platform_www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "pluginId": "org.apache.cordova.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1"
+    "cordova-plugin-whitelist": "1.3.1",
+    "org.apache.cordova.statusbar": "0.1.7"
 };
 // BOTTOM OF METADATA
 });
