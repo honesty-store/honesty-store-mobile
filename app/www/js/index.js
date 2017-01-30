9d48731e53ddefcfbbc21e0212388bbe85939109
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-30 14:32:33 +0000

    Bye bye unwanted comments

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,13 +1,8 @@
 var app = {
-    // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
         document.getElementById('frame').src = 'https://honesty.store';
         universalLinks.subscribe(null, this.didLaunchAppFromLink);
