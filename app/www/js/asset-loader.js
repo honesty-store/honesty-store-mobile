cbf3bc608548569df44a8a104d304a77479d24b1
Sam Burnstone <sburnstone@scottlogic.com>
2017-02-14 12:52:23 +0000

    Basic loading of assets from remote honesty.store

---
+++ app/www/js/asset-loader.js
@@ -0,0 +1,16 @@
+var prefix = 'https://agnostic-history.honesty.store';
+fetch(prefix + '/asset-manifest.json')
+  .then(r => r.json())
+  .then(assets => {
+    var mainJS = assets['main.js'];
+    var mainCSS = assets['main.css'];
+
+    var scriptEl = document.createElement('script');
+    scriptEl.setAttribute('src', prefix + '/' + mainJS);
+    document.body.appendChild(scriptEl);
+
+    var styleEl = document.createElement('link');
+    styleEl.setAttribute('rel', 'stylesheet');
+    styleEl.setAttribute('href', prefix + '/' + mainCSS);
+    document.head.appendChild(styleEl);
+  });
