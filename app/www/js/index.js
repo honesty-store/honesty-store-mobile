148034545172671326337d3e2b9a98f6f069c49f
Sam Burnstone <sburnstone@scottlogic.com>
2017-03-01 11:35:07 +0000

    Use modern syntax
    We can require iOS 10 so we can use const, let, arrow functions e.t.c

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,104 +1,85 @@
-var baseUrl = 'https://honesty.store';
+const baseUrl = 'https://honesty.store';
 
-// Add Promise polyfill
-if (!window.Promise) {
-  window.Promise = Promise;
-}
+const onDeviceReady = () => {
+  // Register for connectivity events
+  document.addEventListener('offline', onOffline);
+  document.addEventListener('online', onOnline);
 
-var app = {
-  initialize: function () {
-    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-  },
+  loadApplicationAssetsIfNeeded();
 
-  onDeviceReady: function () {
-    // Register for connectivity events
-    document.addEventListener("offline", this.onOffline.bind(this));
-    document.addEventListener("online", this.onOnline.bind(this));
+  universalLinks.subscribe(null, didLaunchAppFromLink);
+};
 
-    this.loadApplicationAssetsIfNeeded();
-
-    universalLinks.subscribe(null, this.didLaunchAppFromLink.bind(this));
-  },
-
-  didLaunchAppFromLink: function (eventData) {
-    this.loadApplicationAssetsIfNeeded().then(function() {
+const didLaunchAppFromLink = (eventData) => {
+  loadApplicationAssetsIfNeeded()
+    .then(() => {
       // Pull out url beyond domain
-      var urlPortionRegex = /(honesty\.store\/)(.*)/;
-      var urlMatches = urlPortionRegex.exec(eventData.url);
-
-      console.log(urlMatches);
+      const urlPortionRegex = /(honesty\.store\/)(.*)/;
+      const urlMatches = urlPortionRegex.exec(eventData.url);
 
       if (urlMatches.length !== 3) {
         // Invalid universal link, so just show the main page
         return;
       }
 
-      var newRoute = urlMatches[2];
-      var currentLocationMinusRoute = window.location.href.replace(/(.*index.html)(.*)/, '$1');
+      const newRoute = urlMatches[2];
+      const currentLocationMinusRoute = window.location.href.replace(/(.*index.html)(.*)/, '$1');
       window.location = currentLocationMinusRoute + '#' + newRoute;
       window.location.reload();
     });
-  },
-
-  onOffline: function () {
-    var rootEl = document.getElementById('root');
-    rootEl.classList.add('invisible');
-    var offlineEl = document.getElementById('offline');
-    offlineEl.classList.remove('invisible');
-    console.log('Offline');
-
-    document.documentElement.classList.add('expand-height');
-  },
-
-  onOnline: function () {
-    var rootEl = document.getElementById('root');
-    rootEl.classList.remove('invisible');
-    var offlineEl = document.getElementById('offline');
-    offlineEl.classList.add('invisible');
-
-    document.documentElement.classList.remove('expand-height');
-
-    this.loadApplicationAssetsIfNeeded();
-  },
-
-  loadApplicationAssetsIfNeeded: function () {
-    var hasLoadedAssets = function() {
-      var scripts = document.body.getElementsByTagName('script');
-      for (var i = 0; i < scripts.length; i++) {
-        if (scripts[i].src.match(/honesty\.store\/.*\/main\..*\.js/)) {
-          return true;
-        }
-      }
-      return false;
-    };
-
-    if (hasLoadedAssets()) {
-      return Promise.resolve();
-    }
-
-    return fetch(baseUrl + '/asset-manifest.json')
-      .then(r => r.json())
-      .then(assets => {
-
-        if (hasLoadedAssets()) {
-          /* Protect the odd case where we get multiple calls to load assets in quick succession - we
-          don't want to add mulitple scripts to the same file */
-          return;
-        }
-
-        var mainJS = assets['main.js'];
-        var mainCSS = assets['main.css'];
-
-        var scriptEl = document.createElement('script');
-        scriptEl.setAttribute('src', baseUrl + '/' + mainJS);
-        document.body.appendChild(scriptEl);
-
-        var styleEl = document.createElement('link');
-        styleEl.setAttribute('rel', 'stylesheet');
-        styleEl.setAttribute('href', baseUrl + '/' + mainCSS);
-        document.head.appendChild(styleEl);
-      });
-  }
 };
 
-app.initialize();
+const onOffline = () => {
+  const rootEl = document.getElementById('root');
+  rootEl.classList.add('invisible');
+  const offlineEl = document.getElementById('offline');
+  offlineEl.classList.remove('invisible');
+
+  document.documentElement.classList.add('expand-height');
+};
+
+const onOnline = () => {
+  const rootEl = document.getElementById('root');
+  rootEl.classList.remove('invisible');
+  const offlineEl = document.getElementById('offline');
+  offlineEl.classList.add('invisible');
+
+  document.documentElement.classList.remove('expand-height');
+
+  loadApplicationAssetsIfNeeded();
+};
+
+const loadApplicationAssetsIfNeeded = () => {
+  const hasLoadedAssets = () => document.getElementsByClassName('asset').length > 0;
+
+  if (hasLoadedAssets()) {
+    return Promise.resolve();
+  }
+
+  return fetch(baseUrl + '/asset-manifest.json')
+    .then(r => r.json())
+    .then(assets => {
+
+      if (hasLoadedAssets()) {
+        /* Protect the odd case where we get multiple calls to load assets in quick succession - we
+        don't want to add mulitple scripts to the same file */
+        return;
+      }
+
+      const mainJS = assets['main.js'];
+      const mainCSS = assets['main.css'];
+
+      const scriptEl = document.createElement('script');
+      scriptEl.setAttribute('src', baseUrl + '/' + mainJS);
+      scriptEl.setAttribute('class', 'asset');
+      document.body.appendChild(scriptEl);
+
+      const styleEl = document.createElement('link');
+      styleEl.setAttribute('rel', 'stylesheet');
+      styleEl.setAttribute('href', baseUrl + '/' + mainCSS);
+      document.head.appendChild(styleEl);
+    })
+    .catch(onOffline);
+};
+
+document.addEventListener('deviceready', onDeviceReady, false);
